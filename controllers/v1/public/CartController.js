14f5541396cba3bbb5d2850f8fcec98062cfee85
Jed Bartolo <jedbartolo11@gmail.com>
2022-07-18 15:50:59 +0800

    Get user id through decoded token instead from params of cart and get product details on get cart items

--- controllers/v1/public/CartController.js
+++ controllers/v1/public/CartController.js
@@ -16,8 +16,8 @@ exports.create = catchAsync(async (req, res, next) => {
 
     if (user_cart === null) {
         user_cart = await Cart.create({
-            user_id:  user._id,
-            email:  user.email
+            user_id: user._id,
+            email: user.email
         })
     }
 
@@ -25,16 +25,16 @@ exports.create = catchAsync(async (req, res, next) => {
         product_id: new mongoose.Types.ObjectId(cart_items.product_id),
         cart_id: new mongoose.Types.ObjectId(user_cart._id)
     })
-    
-    if (cart_item === null) { 
+
+    if (cart_item === null) {
         cart_item = await CartItems.create({
-                cart_id: user_cart._id,
-                product_id: cart_items.product_id,
-                quantity: cart_items.quantity,
-                amount: cart_items.amount,
-                total_amount: cart_items.total_amount,
-                image: cart_items.image || ''
-            })
+            cart_id: user_cart._id,
+            product_id: cart_items.product_id,
+            quantity: cart_items.quantity,
+            amount: cart_items.amount,
+            total_amount: cart_items.total_amount,
+            image: cart_items.image || ''
+        })
     } else {
         cart_item.quantity = cart_items.quantity + cart_item.quantity
         cart_item.total_amount = cart_items.total_amount + cart_item.total_amount
@@ -55,9 +55,42 @@ exports.getCart = catchAsync(async (req, res, next) => {
 
     // user_id has type of mongoose.Schema.ObjectId, in order to get result, we need to convert it to ObjectId
     user_id = new mongoose.Types.ObjectId(user_id);
+
     const user_cart = await Cart
-        .aggregate([{ $match: { user_id: { $eq: user_id } } }])
-        .lookup({ from: 'cart_items', localField: '_id', foreignField: 'cart_id', as: 'cart_items' });
+        .aggregate([
+            { $match: { user_id: { $eq: user_id } } },
+            {
+                "$lookup": {
+                    // foreignField is located in the from table
+                    from: 'cart_items', localField: '_id', foreignField: 'cart_id', as: 'cart_items',
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: "products",
+                                localField: "product_id",
+                                foreignField: "_id",
+                                as: "product"
+                            },
+                        },
+                    ],
+                    as: "cart_items"
+                },
+
+            },
+
+            {
+                $project: {
+                    "_id": 0,
+                    "cart_items._id": 1,
+                    "cart_items.amount": 1,
+                    "cart_items.total_amount": 1,
+                    "cart_items.quantity": 1,
+                    "cart_items.product.name": 1,
+                    "cart_items.product.images": 1,
+                }
+            }
+
+        ])
 
     return res.status(200).json({
         status: 'success',
