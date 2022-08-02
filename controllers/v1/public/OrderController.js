3d3b7ac8b45a6c7ed873e50d58f7e3968823ccc4
Jed Bartolo <jedbartolo11@gmail.com>
2022-08-02 17:40:30 +0800

    Restrict ordering items not on cart_items or orders that were already placed

--- controllers/v1/public/OrderController.js
+++ controllers/v1/public/OrderController.js
@@ -1,6 +1,7 @@
 const Order = require(`${process.cwd()}/models/ordersModel`)
 const Product = require(`${process.cwd()}/models/productModel`)
 const Cart = require(`${process.cwd()}/models/cartModel`)
+const CartItems = require(`${process.cwd()}/models/cartItemsModel`)
 
 const AppError = require(`${process.cwd()}/handlers/AppError`)
 
@@ -28,22 +29,49 @@ exports.placeOrder = catchAsync(async (req, res, next) => {
     const { user } = req
     const { notes, delivery_address } = req.body
     let { ordered_items } = req.body
-
+    let product = {}
     const name = user.first_name + user.last_name
 
     let user_id = user._id
-    user_id = new mongoose.Types.ObjectId(user_id);
+    user_id = new mongoose.Types.ObjectId(user_id)
 
+    let user_cart = await Cart.findOne({
+        user_id: user_id
+    })
+    
     let ref_num = Date.now().toString(36) + Math.random().toString(36).substr(6)
     ref_num = process.env.ORDER_PREFIX + ref_num.toUpperCase(ref_num)
 
     let total_amount = await getOrderTotalAmount(ordered_items)
 
-    let product = {}
+    let already_ordered = false
 
-    let user_cart = await Cart.findOne({
-        user_id: user_id
-    })
+    if (user_cart === null) already_ordered = true
+
+
+    let cart_item = {}
+
+    // Check if this order is already placed
+    if (user_cart !== null && ! already_ordered) {
+        const cart_id = new mongoose.Types.ObjectId(user_cart._id)
+        for (const order_item of ordered_items) {
+            cart_item = await CartItems.findOne({
+                cart_id: cart_id,
+                product_id: new mongoose.Types.ObjectId(order_item.product_id)
+            })
+            console.log('already_ordered', cart_item)
+            if (cart_item === null) {
+                already_ordered = true
+                break
+            } 
+        }
+        
+    }
+
+
+    if (already_ordered) {
+        throw new AppError('This cart is already ordered!')
+    }
 
     for (const item of ordered_items) {
         item.product_id = new mongoose.Types.ObjectId(item.product_id)
